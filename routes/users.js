7a6c6e524fb37bed5c0568ed3819127924a6c98a
phillipashe <ashephillip@gmail.com>
2022-07-17 12:06:24 -0400

    set up routes

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('User List');
+});
+
+router.get('/new', (req, res) => {
+  res.send('User New Form')
+});
+
+router.post('/', (req, res) => {
+  res.send('Create user');
+})
+
+router.get('/:id', (req, res) => {
+  res.send(`Got user ${req.params.id}`);
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+const userRouter = require('./routes/users');
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {text: 4});
+  res.render('index', { text: 4 });
 });
+
+app.use('/users', userRouter);
 
 app.listen(8080);

848ddb38ba848c4b91f32980098e3432c7274efc
phillipashe <ashephillip@gmail.com>
2022-07-17 11:07:11 -0400

    ejs setup

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {text: 4});
+});
+
+app.listen(8080);
